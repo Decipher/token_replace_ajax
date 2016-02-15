ee387a3515f5bf304dc331fa7f786a4edecc3e06
Stuart Clark <stu@rtclark.net>
2016-02-16 10:17:28 +1100

    Initial D8 port.

--- token_replace_ajax.api.php
+++ token_replace_ajax.api.php
@@ -8,18 +8,19 @@
 /**
  * Allows modules to retrieve their entity form the provided form/form_state.
  *
- * @param $entity_type
+ * @param string $entity_type
  *   The entity type of the token being processed.
- * @param $form
+ * @param array $form
  *   The form posted to Token replace AJAX callback.
- * @param $form_state
+ * @param \Drupal\Core\Form\FormStateInterface $form_state
  *   The processed form state of the Token replace AJAX form submission.
  *
  * @return bool|object
+ *   The entity if found, otherwise FALSE.
  */
-function hook_token_replace_ajax_form_entity($entity_type, $form, $form_state) {
+function hook_token_replace_ajax_form_entity($entity_type, $form, \Drupal\Core\Form\FormStateInterface $form_state) {
   if (isset($form['#my_entity'])) {
-    $my_entity = (object) array_merge((array) $form_state['my_entity'], isset($form_state['values']['my_entity']) ? $form_state['values']['my_entity'] : array());
+    $my_entity = (object) array_merge((array) $form_state['my_entity'], isset($form_state['values']['my_entity']) ? $form_state['values']['my_entity'] : []);
 
     return $my_entity;
   }
@@ -28,13 +29,13 @@ function hook_token_replace_ajax_form_entity($entity_type, $form, $form_state) {
 }
 
 /**
- * Allows modules to alter the response value of the Token replace AJAX callback..
+ * Allows modules to alter the response value of the Token replace AJAX call.
  *
- * @param $value
+ * @param string $value
  *   The processed token result.
- * @param $token
+ * @param string $token
  *   The unprocessed token.
- * @param $data
+ * @param array $data
  *   The data used for the token replacement.
  */
 function hook_token_replace_ajax_response_alter(&$value, $token, $data) {
